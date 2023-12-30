aeeae3b52f3ca6ed744c3f91a6d369d589add16c
rashidulislam71 <jrashidulislam0102@gmail.com>
2023-12-30 23:37:46 +0600

    signup form

--- src/Pages/Auth/Sign up/SignUp.jsx
+++ src/Pages/Auth/Sign up/SignUp.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import SocialSignUp from "../SocialSignUp/SocialSignUp";
+import "../../PagesCSS/Pages.css"
+import "../../../Component/MainLayout/Common/Common.css"
 
-function SignUp() {
+const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="signUpPageParent">
+      <div className="signUpPageInfo">
+        <div className="signUpForm">
+          <div className="title">
+            <h1>Registration</h1>
+            <hr />
+            <div className="haveAccount">
+              <p>
+                Already Have An Account? <span>Log In</span>
+              </p>
+            </div>
+          </div>
 
-export default SignUp
+          <div className="formInfo">
+            <form>
+              <label>Full Name:</label> <br />
+              <input
+                type="text"
+                name="fullName"
+                id="fullName"
+                placeholder="Full Name"
+              />{" "}
+              <br />
+              <label>Email:</label> <br />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+              />{" "}
+              <br />
+              <label>Password:</label> <br />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />{" "}
+              <br />
+              <label>Confirm Password:</label> <br />
+              <input
+                type="password"
+                name="ConfirmPassword"
+                id="ConfirmPassword"
+                placeholder="Confirm Password."
+              />
+            </form>
+            <div className="submitBtn">
+              <button>REGISTRATION</button>
+            </div>
+          </div>
+          <SocialSignUp />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
---
+++ src/Pages/Auth/SocialSignUp/SocialSignUp.jsx
@@ -0,0 +1,33 @@
+
+
+const SocialSignUp = () => {
+    return (
+
+
+        <div>
+            <div className="socialSignUpInfo">
+                <div className="social">
+                    <div className="google">
+                        <div className="logo">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="text">
+                            <button>Sign Up With Google</button>
+                        </div>
+                    </div>
+
+                    <div className="gitHub">
+                        <div className="logo">
+                            <img src="" alt="" />
+                        </div>
+                        <div className="text">
+                            <button>Sign Up With GitHub</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SocialSignUp;
