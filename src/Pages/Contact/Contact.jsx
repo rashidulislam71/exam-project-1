fb3b4ccab086d52b8d9be9249f2212c2e328140b
rashidulislam71 <jrashidulislam0102@gmail.com>
2024-01-01 11:07:09 +0600

    contact page design

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
-import "../PagesCSS/Pages.css"
+import React from "react";
+import "../PagesCSS/Pages.css";
 
 const Contact = () => {
   return (
-    <div>
-      <div className="contactPage">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate nihil asperiores quasi cum exercitationem provident dolorem et dolore eveniet aliquam, commodi esse placeat optio laudantium explicabo porro fuga obcaecati? Facere, saepe! Laudantium doloribus sed ut nesciunt cumque veniam soluta aliquam reiciendis, quidem eum tempore praesentium aut quisquam iste ullam iure placeat similique perspiciatis quibusdam atque fugiat officia consequatur hic! Eaque perferendis deserunt aliquid? Doloribus minus quam voluptatum cupiditate nemo deserunt enim eius illum, quasi magni. Eius aliquam quod earum soluta dicta, recusandae sit exercitationem, provident voluptas corrupti repellendus quia fugiat adipisci totam facilis molestias consectetur optio nostrum nesciunt enim? Nostrum cumque nulla alias aliquid delectus, voluptatibus culpa quidem repudiandae nesciunt omnis! Rerum repellendus fuga maxime at est cumque deserunt eligendi, minima dignissimos voluptates dolorem, assumenda atque? Modi quasi corporis voluptate ratione quaerat labore. Est sint minus laboriosam ea odio. Aspernatur nostrum iure consectetur nihil vero itaque ab earum rem in libero optio omnis obcaecati, consequuntur facilis? Quasi exercitationem tenetur sit accusamus ipsa harum est molestiae commodi, soluta illo possimus officiis accusantium dolorem ducimus aliquam tempore magni minima? Error eum repellat odit minus deserunt quaerat repellendus, inventore sequi tempore modi quae molestias commodi maiores labore quasi aliquam, ducimus, odio beatae ullam necessitatibus accusantium dignissimos dolore nisi! Repudiandae, quas perspiciatis. Veniam culpa expedita at libero quod, impedit repellat sequi, recusandae quaerat cum ut! Dignissimos et eveniet deserunt natus quae quo voluptatum iste nobis facere. Omnis, sed? Ea magni nisi beatae quod modi atque non veniam! Doloribus eligendi voluptatibus odit recusandae repellendus reprehenderit autem soluta nobis at, nam eveniet quae aperiam nostrum quas dolorem ipsam tempore. Vero, harum. Rerum aliquid, molestiae repudiandae voluptatum optio quo cupiditate, impedit cumque excepturi omnis incidunt? Blanditiis earum eos consectetur atque vel aperiam molestias, veniam aliquid nemo, quos fugit, neque ducimus necessitatibus! In voluptas accusamus esse laboriosam ab.</p>
+    <div className="contactPage">
+      <div className="contact-page">
+        <div className="contact-header">
+          <h1>Contact Us</h1>
+          <p>
+            Have any questions about job opportunities or our services? Reach
+            out to us!
+          </p>
+        </div>
+
+        <div className="contact-form">
+          <form>
+            <div className="form-group">
+             
+              <input
+                type="text"
+                id="ConName"
+                name="name"
+                placeholder="Your Name"
+                autoFocus
+                required
+              />
+            </div>
+            <div className="form-group">
+              
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Your Email"
+                required
+              />
+            </div>
+            <div className="form-group">
+             <label htmlFor="message">Message!</label>
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Please explain your message!"
+                required
+              ></textarea>
+            </div >
+            <div className="sendBtn">
+            <button type="submit">Send Massage</button>
+            </div>
+            
+          </form>
+        </div>
+
+        <div className="jobs-paragraph">
+          <h2>Explore Exciting Job Opportunities</h2>
+          <p>
+            Discover your dream job on our platform! Whether you're a seasoned
+            professional or just starting your career journey, we offer a
+            diverse range of job listings across various industries. Our mission
+            is to connect talented individuals with rewarding career
+            opportunities. Your next job might be just a click away. Join us in
+            shaping your future and advancing your professional growth.
+          </p>
+        </div>
       </div>
     </div>
   );
